b840f55e64fdf4161944c3d98d691401b7096a37
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-12-20 10:49:29 -0300

    implantado renderizacao dos cards na lista de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -2,8 +2,11 @@ import Banner from "components/Banner";
 import styles from "./Favoritos.module.css";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
+import { useFavoritoContext } from "contextos/Favoritos";
 
 function Favoritos() {
+  const { favorito } = useFavoritoContext();
+
   return (
     <>
       <Banner imagem="favoritos" />
@@ -11,11 +14,9 @@ function Favoritos() {
         <h1>Meus Favoritos</h1>
       </Titulo>
       <section className={styles.container}></section>
-      <Card
-        id="2"
-        titulo="Gato bonifácio"
-        capa="https://thecatapi.com/api/images/get?format=src&type=png"
-      />
+      {favorito.map((fav) => {
+        return <Card {...fav} key={fav.id} />;
+      })}
     </>
   );
 }
